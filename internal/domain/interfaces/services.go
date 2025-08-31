2176f59b250eb88e5937e17f10a360d4608e413c
blackprince001 <appiahboaduprince@gmail.com>
2025-08-31 22:40:37 +0000

    feat: define interfaces for embedding and similarity services

---
+++ internal/domain/interfaces/services.go
@@ -0,0 +1,31 @@
+package interfaces
+
+import (
+	"context"
+	"time"
+
+	"teiwrappergolang/internal/domain/entities"
+)
+
+type EmbeddingService interface {
+	Embed(ctx context.Context, req *entities.EmbedRequest) (*entities.EmbedResponse, error)
+	EmbedAll(ctx context.Context, req *entities.EmbedAllRequest) (*entities.EmbedAllResponse, error)
+	EmbedSparse(ctx context.Context, req *entities.EmbedSparseRequest) (*entities.EmbedSparseResponse, error)
+}
+
+type SimilarityService interface {
+	CalculateSimilarity(ctx context.Context, req *entities.SimilarityRequest) (*entities.SimilarityResponse, error)
+}
+
+type ClientService interface {
+	EmbeddingService
+	SimilarityService
+}
+
+type HTTPClient interface {
+	Get(ctx context.Context, endpoint string) ([]byte, error)
+	Post(ctx context.Context, endpoint string, body any) ([]byte, error)
+	PostRaw(ctx context.Context, endpoint string, body []byte, contentType string) ([]byte, error)
+	SetTimeout(timeout time.Duration)
+	Close() error
+}
